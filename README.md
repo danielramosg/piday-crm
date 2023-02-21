003c2c9d0b123c7df4a01a7b4105d4f92c478eae
Daniel Ramos <daniel.ramosg@gmail.com>
2023-02-21 01:18:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pi Day - CRM
+
+Website for the Pi Day, celebrated by Centre de Recerca Matemàtica.
+
+## Author
+
+Daniel Ramos
---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+import ready from 'document-ready';
+import renderMathInElement from '../../node_modules/katex/dist/contrib/auto-render';
+
+function main() {
+  Array.from(document.getElementsByClassName('num-printed-digits')).forEach(
+    (element) => {
+      element.innerHTML = "NUM-DIGITS";
+    },
+  );
+
+  renderMathInElement(document.body, {
+    // customised options
+    // • auto-render specific keys, e.g.:
+    delimiters: [
+      { left: '$$', right: '$$', display: true },
+      { left: '$', right: '$', display: false },
+      { left: '\\(', right: '\\)', display: false },
+      { left: '\\[', right: '\\]', display: true },
+    ],
+    // • rendering keys, e.g.:
+    throwOnError: false,
+  });
+}
+
+ready(main);
