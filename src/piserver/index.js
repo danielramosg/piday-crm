2a3891b7d7982474792481e4befa80a1a0c9c615
Daniel Ramos <daniel.ramosg@gmail.com>
2023-03-07 11:50:53 +0100

    Server response as json object

--- src/piserver/index.js
+++ src/piserver/index.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 /**
  * Download the file with a billion digits of pi from
  * https://stuff.mit.edu/afs/sipb/contrib/pi/
+ *
+ * To test: last digits of pi-billion.txt are '5275045519'
  */
 
 // const buffer = new SharedArrayBuffer(1000000002);
@@ -19,23 +21,44 @@ app.listen(3000, () => {
   console.log('Listening at port 3000');
 });
 
-// app.get('/search/:digits', (request, response) => {
-//   console.log(request.params.digits);
-//   response.send(request.params.digits);
-// });
-
-// fs.readFile('../digits/pi-billion.txt', (error, buffer) => {
-//   const i = buffer.indexOf('5275045519');
-//   console.log(buffer.slice(buffer.length - 10, buffer.length).toString());
-//   console.log(i - 1);
-//   console.log(buffer.slice(i - 50, i + 50).toString());
-// });
-
 fs.readFile('../digits/pi-billion.txt', (error, buffer) => {
   console.log('Billion digits of pi loaded');
   // buffer is a binary array, not a string.
+
+  const searchItUp = (str) => {
+    const R = 50;
+    const i = buffer.indexOf(str);
+    if (i < 0) {
+      return { found: false, str: str };
+    }
+
+    const nbh = buffer
+      .slice(Math.max(i - R, 0), Math.min(i + str.length + R, buffer.length))
+      .toString();
+    const prev = buffer
+      .slice(Math.max(i - R, 0), i)
+      .toString()
+      .padStart(R, ' ');
+    const post = buffer
+      .slice(i + str.length, Math.min(i + str.length + R, buffer.length))
+      .toString()
+      .padEnd(R, ' ');
+
+    return {
+      found: true,
+      str: str,
+      index: (i - 1).toString(),
+      neighborhood: nbh,
+      prev: prev,
+      post: post,
+    }; // keep in mind that the file starts with '3.'
+  };
+
   app.get('/search/:digits', (request, response) => {
-    const i = buffer.indexOf(request.params.digits);
-    response.send((i - 1).toString());
+    response.type('json');
+    response.set('Access-Control-Allow-Origin', '*');
+    const location = searchItUp(request.params.digits);
+    // console.log(location);
+    response.send(location);
   });
 });
